a91a3a08a1ecfa352b6e70213ea0d74512ff7217
noelberistain <noelberistain@gmail.com>
2019-12-18 13:29:46 -0800

    slices of 5 in to the application has been added, button is showing the next 5 or remaining items from the data still quirks but the main function is workign

--- src/Tasks.js
+++ src/Tasks.js
@@ -6,18 +6,32 @@ class Tasks extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      start: 0,
+      end: 5,
       tasks: [],
     };
   }
 
   componentDidMount() {
-    this.setState({ tasks: [...data] });
+    const showing = this.slicing();
+    const { end } = this.state;
+    this.setState({ tasks: [...showing], start: end, end: end + 5 });
   }
+
+  slicing = () => {
+    const { start, end } = this.state;
+    const showing = [...data].slice(start, end);
+    return showing;
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
+    const { end } = this.state;
+    const showing = this.slicing();
+    this.setState({ tasks: [...showing], start: end, end: end + 5 });
   };
+
   onChecked = (id, status) => {
-    console.log(id, status);
     const helper = this.state.tasks.map((task) => {
       if (task.id === id) {
         let aux = { ...task };
@@ -26,12 +40,12 @@ class Tasks extends Component {
       }
       return task;
     });
-
     this.setState({ tasks: [...helper] });
   };
+
   render() {
-    const { tasks } = this.state;
-    console.log(tasks);
+    const { tasks, start, end } = this.state;
+    console.log(start, end);
     return (
       <>
         <List tasks={tasks} onChecked={this.onChecked} />
